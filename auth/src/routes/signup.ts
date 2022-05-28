cf06c948e7af0b81211a7ae7e0023673ab4158b4
Radostin Marinov <r.marinov045@gmail.com>
2022-05-29 01:39:35 +0300

    Integrating custom error handling middleware to express app

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,31 +1,34 @@
-import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 const router = express.Router();
 
-router.post('/api/users/signup', [
-    body('email')
-        .isEmail()
-        .withMessage('Email is not valid'),
-    body('password')
-        .trim()
-        .isLength({ min: 4, max: 20 })
-        .withMessage('Password must be between 4 and 20 characters.')
-],
-    (req: Request, res: Response) => {
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email is not valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters."),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
 
-        const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
 
-        if (!errors.isEmpty()) {
-            return res.status(400).send(errors.array());
-        }
+    const { email, password } = req.body;
 
-        const { email, password } = req.body;
+    console.log("Creating user....");
 
-        console.log('Creating user....');
+    throw new DatabaseConnectionError();
 
-        res.send({});
-
-    });
+    res.send({});
+  }
+);
 
 export { router as signupRouter };
