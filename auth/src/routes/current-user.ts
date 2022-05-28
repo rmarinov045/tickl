84511e20ed926e3fccfe5fec34179374a5277c08
Radostin Marinov <r.marinov045@gmail.com>
2022-05-29 00:58:10 +0300

    Adding separate route handlers and initial config

---
+++ auth/src/routes/current-user.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/api/users/currentUser', (req, res) => {
+    res.send('Hello there!');
+});
+
+export { router as currentUserRouter };
