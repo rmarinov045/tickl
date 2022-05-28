fd45a5795df42c3a553aabe87ece61768dad9251
Radostin Marinov <r.marinov045@gmail.com>
2022-05-29 01:38:42 +0300

    Adding custom error handling middleware

---
+++ auth/src/middlewares/error-handler.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof RequestValidationError) {
+    console.log("Handling this an error as a request validation error");
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    console.log('Handling this error as a DB connection error');
+  }
+
+  res.status(400).send({
+    message: err.message,
+  });
+};
