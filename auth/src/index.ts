5d32ec6de2575322dcb30ab088da41b41b40bb40
Radostin Marinov <r.marinov045@gmail.com>
2022-05-29 16:09:35 +0300

    Adding partial signup implementation

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,7 +8,7 @@ import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-errors';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
