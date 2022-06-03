9770ef332b8bbf6751f08880469671b5200b65a5
Radostin Marinov <r.marinov045@gmail.com>
2022-06-03 23:20:42 +0300

    Adding unit tests

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,11 +1,18 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
 import { app } from '../app';
+
+declare global {
+	var signin: () => Promise<string[]>;
+}
 
 let mongo: any;
 
 beforeAll(async () => {
-	const mongo = new MongoMemoryServer();
+	process.env.JWT_KEY = 'test';
+
+	mongo = await MongoMemoryServer.create();
 	const mongoUri = mongo.getUri();
 
 	await mongoose.connect(mongoUri);
@@ -23,3 +30,20 @@ afterAll(async () => {
 	await mongo.stop();
 	await mongoose.connection.close();
 });
+
+global.signin = async () => {
+	const email = 'test@test.com';
+	const password = 'password';
+
+	const response = await request(app)
+		.post('/api/users/signup')
+		.send({
+			email,
+			password,
+		})
+		.expect(201);
+
+	const cookie = response.get('Set-Cookie');
+
+	return cookie;
+};
