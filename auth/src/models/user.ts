aa4e5cc0277792b50c51038e66c7869b3859efab
Radostin Marinov <r.marinov045@gmail.com>
2022-06-03 20:20:28 +0300

    Adding transform functionality to user schema: - Removing password and version keys - Remapping MongoDB id key to plain 'id' key for consistency

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -21,16 +21,28 @@ interface UserDoc extends mongoose.Document {
 	password: string;
 }
 
-const userSchema = new mongoose.Schema({
-	email: {
-		type: String,
-		required: true,
+const userSchema = new mongoose.Schema(
+	{
+		email: {
+			type: String,
+			required: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
 	},
-	password: {
-		type: String,
-		required: true,
-	},
-});
+	{
+		toJSON: {
+			transform(doc, ret) {
+				ret.id = ret._id;
+				delete ret._id;
+				delete ret.password;
+				delete ret.__v;
+			},
+		},
+	}
+);
 
 userSchema.pre('save', async function (done) {
 	// 'this' refers to user that is being saved instead of document, since function is not arrow
