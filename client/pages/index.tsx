18db1947a67ac152e4d32fa00734ea6462ff2ff3
Radostin Marinov <r.marinov045@gmail.com>
2022-06-04 20:07:50 +0300

    Add api call for current user

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import buildAxiosClient from '../api/build-axios-client';
 
-function index() {
-  return (
-    <div>index</div>
-  )
-}
+const LandingPage = ({
+	currentUser,
+}: {
+	currentUser: { id: string; email: string } | null;
+}) => {
+	return (
+		<div className='container'>
+			Homepage
+			<h2>{currentUser?.email || 'test'}</h2>
+		</div>
+	);
+};
 
-export default index
+LandingPage.getInitialProps = async (context) => {
+	const client = buildAxiosClient(context);
+	const { data } = await client.get('/api/users/currentuser');
+
+	return data;
+};
+
+export default LandingPage;
