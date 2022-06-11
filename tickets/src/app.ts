e5dddb575e49976b66eba83f724c4144b42e7be2
Radostin Marinov <r.marinov045@gmail.com>
2022-06-11 21:34:25 +0300

    Add router for new ticket route and add current user middleware

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,7 +3,9 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 
-import { errorHandler, NotFoundError } from '@rmtickl/common';
+import { errorHandler, NotFoundError, currentUser } from '@rmtickl/common';
+
+import { CreateTicketRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,6 +16,10 @@ app.use(
 		secure: process.env.NODE_ENV !== 'test',
 	})
 );
+
+app.use(currentUser);
+
+app.use(CreateTicketRouter);
 
 app.all('*', () => {
 	throw new NotFoundError();
