5ef529bee21377edf3243de4b8af62286c30dc9b
Radostin Marinov <r.marinov045@gmail.com>
2022-06-16 20:16:29 +0300

    Add save ticket functionality

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 
 it('has a route handler listening to /api/tickets for post request', async () => {
 	const response = await request(app).post('/api/tickets').send({});
@@ -58,12 +59,24 @@ it('returns an error if an invalid price is provided', async () => {
 		.expect(400);
 });
 
-it('created a ticket with valid input', async () => {
+it('creates a ticket with valid input', async () => {
+	let tickets = await Ticket.find({});
+
+	expect(tickets.length).toEqual(0);
+
+	const title = 'test';
+
 	await request(app)
 		.post('/api/tickets')
+		.set('Cookie', global.signin())
 		.send({
-			title: 'test',
+			title: title,
 			price: 20,
 		})
 		.expect(201);
+
+	tickets = await Ticket.find({});
+	expect(tickets.length).toEqual(1);
+	expect(tickets[0].price).toEqual(20);
+	expect(tickets[0].title).toEqual(title);
 });
