504f0cdc6ba1f630924e693482a40ab454064b5f
Radostin Marinov <r.marinov045@gmail.com>
2022-06-20 22:43:54 +0300

    Fix typo in ticket deployment config & fix typo in ticket service start function

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -33,7 +33,7 @@ const start = async () => {
 	}
 
 	app.listen(3000, () => {
-		console.log('Auth service listening on port 3000....');
+		console.log('TIcketing service listening on port 3000....');
 	});
 };
 
