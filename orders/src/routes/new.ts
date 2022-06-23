413adca10c452cf1fdb36476b5d97f923d367e03
Radostin Marinov <r.marinov045@gmail.com>
2022-06-23 18:23:05 +0300

    Add create order controller

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -13,6 +13,8 @@ import { Order } from '../models/order';
 
 const router = express.Router();
 
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
 router.post(
 	'/api/orders',
 	requireAuth,
@@ -39,7 +41,21 @@ router.post(
 			throw new BadRequestError('Ticket is already reserved');
 		}
 
-		res.send({});
+		const expiration = new Date();
+		expiration.setSeconds(
+			expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS
+		);
+
+		const order = Order.build({
+			userId: req.currentUser!.id,
+			status: OrderStatus.Created,
+			expiresAt: expiration,
+			ticket,
+		});
+
+		await order.save();
+
+		res.status(201).send(order);
 	}
 );
 
