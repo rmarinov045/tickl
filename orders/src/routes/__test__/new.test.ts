9c80fe5b5234b2e12f65722098c85cb45f389089
Radostin Marinov <r.marinov045@gmail.com>
2022-06-25 22:07:50 +0300

    Add event publishing unit tests

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -3,6 +3,7 @@ import { app } from '../../app';
 import mongooose from 'mongoose';
 import { Order, OrderStatus } from '../../models/order';
 import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('returns an error if the ticket does not exist', async () => {
 	const ticketId = new mongooose.Types.ObjectId().toHexString();
@@ -59,4 +60,21 @@ it('reserves a ticket', async () => {
 		.expect(201);
 });
 
-it.todo('emits an order created event');
+it('emits an order created event', async () => {
+	const ticket = Ticket.build({
+		title: 'test',
+		price: 20,
+	});
+
+	await ticket.save();
+
+	await request(app)
+		.post('/api/orders')
+		.set('Cookie', global.signin())
+		.send({
+			ticketId: ticket.id,
+		})
+		.expect(201);
+
+	expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
