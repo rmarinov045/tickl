7af73d8af6bb1ac7052e99804282a2160ef66536
Radostin Marinov <r.marinov045@gmail.com>
2022-06-28 19:12:08 +0300

    Add ticket updated listener unit test and fix typo in ticket findByEvent method

---
+++ orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+import { TicketUpdatedEvent } from '@rmtickl/common';
+import { TicketUpdatedListener } from '../ticket-updated-listener';
+import { natsWrapper } from '../../../nats-wrapper';
+import { Ticket } from '../../../models/ticket';
+import { Message } from 'node-nats-streaming';
+
+const setup = async () => {
+	const listener = new TicketUpdatedListener(natsWrapper.client);
+	const ticket = Ticket.build({
+		title: 'test',
+		price: 20,
+		id: new mongoose.Types.ObjectId().toHexString(),
+	});
+	await ticket.save();
+	const data: TicketUpdatedEvent['data'] = {
+		id: ticket.id,
+		version: ticket.version + 1,
+		title: 'new test',
+		price: 30,
+		userId: new mongoose.Types.ObjectId().toHexString(),
+	};
+	// @ts-ignore
+	const msg: Message = {
+		ack: jest.fn(),
+	};
+
+	return { listener, data, msg, ticket };
+};
+
+it('finds, updates and saves a ticket', async () => {
+	const { msg, data, ticket, listener } = await setup();
+
+	await listener.onMessage(data, msg);
+
+	const updatedTicket = await Ticket.findById(ticket.id);
+
+	expect(updatedTicket!.title).toEqual(data.title);
+	expect(updatedTicket!.price).toEqual(data.price);
+	expect(updatedTicket!.version).toEqual(data.version);
+});
+
+it('acks the message', async () => {
+	const { msg, data, listener } = await setup();
+
+	await listener.onMessage(data, msg);
+
+	expect(msg.ack).toHaveBeenCalled();
+});
--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -51,7 +51,7 @@ ticketSchema.plugin(updateIfCurrentPlugin);
 ticketSchema.statics.findByEvent = (event: { id: string; version: number }) => {
 	return Ticket.findOne({
 		_id: event.id,
-		version: event.version,
+		version: event.version - 1,
 	});
 };
 
