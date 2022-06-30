b04f3951b1430370258366c900acf4c0ab8ee42e
Radostin Marinov <r.marinov045@gmail.com>
2022-06-30 17:13:54 +0300

    Add order created listener

---
+++ payments/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,23 @@
+import { Listener, Subjects, OrderCreatedEvent } from '@rmtickl/common';
+import { Message } from 'node-nats-streaming';
+import { Order } from '../../models/order';
+import { queueGroupName } from './queue-group-name';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+	subject: Subjects.OrderCreated = Subjects.OrderCreated;
+	queueGroupName = queueGroupName;
+
+	async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+		const order = Order.build({
+			id: data.id,
+			price: data.ticket.price,
+			status: data.status,
+			userId: data.userId,
+			version: data.version,
+		});
+
+		await order.save();
+
+		msg.ack();
+	}
+}
