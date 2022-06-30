4bfcfaab0c72f2c81498e355bf2477e735ec1fac
Radostin Marinov <r.marinov045@gmail.com>
2022-06-30 17:57:45 +0300

    Link controller to app

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -2,8 +2,8 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-
 import { errorHandler, NotFoundError, currentUser } from '@rmtickl/common';
+import { createChargeRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -16,6 +16,7 @@ app.use(
 );
 
 app.use(currentUser);
+app.use(createChargeRouter);
 
 app.all('*', () => {
 	throw new NotFoundError();
