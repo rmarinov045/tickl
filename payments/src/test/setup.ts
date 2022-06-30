2a529c1c2a37f72ab90d4162bebc9ef1c2450e6b
Radostin Marinov <r.marinov045@gmail.com>
2022-06-30 18:20:42 +0300

    Add optional userId arg to sign in function for unit tests

--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
 
 declare global {
-	var signin: () => string[];
+	var signin: (id?: string) => string[];
 }
 
 jest.mock('../nats-wrapper');
@@ -33,9 +33,9 @@ afterAll(async () => {
 	await mongoose.connection.close();
 });
 
-global.signin = () => {
+global.signin = (id?: string) => {
 	const payload = {
-		id: new mongoose.Types.ObjectId().toHexString(),
+		id: id || new mongoose.Types.ObjectId().toHexString(),
 		email: 'test@test.com',
 	};
 
