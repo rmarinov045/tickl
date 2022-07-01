b7f561b877c3f93efcb5776fe979e51170fba2c4
Radostin Marinov <r.marinov045@gmail.com>
2022-07-01 16:46:33 +0300

    Add additional unit tests

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -3,7 +3,10 @@ import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
 import { Order } from '../../models/order';
+import { Payment } from '../../models/payment';
 import { stripe } from '../../stripe';
+
+jest.mock('../../stripe');
 
 it('returns a 404 when purchasing an order that does not exist', async () => {
 	await request(app)
@@ -59,8 +62,8 @@ it('returns a 400 when purchasing a cancelled order', async () => {
 		})
 		.expect(400);
 });
-
-it('returns a 204 with valid input', async () => {
+// Below requires real API key and stripe API calls
+it.skip('returns a 201 with valid input', async () => {
 	const userId = new mongoose.Types.ObjectId().toHexString();
 	const price = Math.floor(Math.random() * 100000);
 	const order = Order.build({
@@ -89,4 +92,42 @@ it('returns a 204 with valid input', async () => {
 
 	expect(stripeCharge).toBeDefined();
 	expect(stripeCharge!.currency).toEqual('usd');
+});
+
+it.skip('persist new payment in DB', async () => {
+	const userId = new mongoose.Types.ObjectId().toHexString();
+	const price = Math.floor(Math.random() * 100000);
+	const order = Order.build({
+		id: new mongoose.Types.ObjectId().toHexString(),
+		userId: userId,
+		version: 0,
+		price,
+		status: OrderStatus.Created,
+	});
+
+	await order.save();
+
+	await request(app)
+		.post('/api/payments')
+		.set('Cookie', global.signin(userId))
+		.send({
+			token: 'tok_visa',
+			orderId: order.id,
+		})
+		.expect(201);
+
+	const stripeCharges = await stripe.charges.list({ limit: 50 });
+	const stripeCharge = stripeCharges.data.find(
+		(charge) => charge.amount === price * 100
+	);
+
+	expect(stripeCharge).toBeDefined();
+	expect(stripeCharge!.currency).toEqual('usd');
+
+	const payment = await Payment.findOne({
+		orderId: order.id,
+		stripeId: stripeCharge!.id,
+	});
+
+	expect(payment).not.toBeNull();
 });
