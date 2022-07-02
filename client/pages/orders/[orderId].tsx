e71a38ab0e4ce2a6ff04be2a1d0ccf50fa0d8c51
Radostin Marinov <r.marinov045@gmail.com>
2022-07-02 16:19:38 +0300

    Add time left until order expires timer

--- client/pages/orders/[orderId].tsx
+++ client/pages/orders/[orderId].tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { TicketData } from '..';
 
 export interface OrderData {
@@ -9,8 +9,33 @@ export interface OrderData {
 	id: string;
 }
 
-function OrderShow() {
-	return <div>OrderShow</div>;
+function OrderShow({ order }: { order: OrderData }) {
+	const [timeLeft, setTimeLeft] = useState(0);
+
+	useEffect(() => {
+		const findTimeLeft = () => {
+			// @ts-ignore
+			const msLeft = new Date(order.expiresAt) - new Date();
+			setTimeLeft(Math.round(msLeft / 1000));
+		};
+		findTimeLeft();
+		const timerId = setInterval(findTimeLeft, 1000);
+
+		return () => clearInterval(timerId);
+	}, []);
+
+	if (timeLeft < 0) {
+		return <div>Order expired</div>;
+	}
+
+	return <div>Time left to pay: {timeLeft} seconds</div>;
 }
+
+OrderShow.getInitialProps = async (context, client) => {
+	const { orderId } = context.query;
+	const { data } = await client.get(`/api/orders/${orderId}`);
+
+	return { order: data };
+};
 
 export default OrderShow;
