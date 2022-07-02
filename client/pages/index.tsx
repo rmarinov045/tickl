35b598f14ba848320b28b27452b8e02d8e197e35
Radostin Marinov <r.marinov045@gmail.com>
2022-07-02 15:16:13 +0300

    Add ticket table to index page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,19 +1,48 @@
 import React from 'react';
 
+interface Ticket {
+	title: string;
+	price: number;
+	version: number;
+	id: string;
+}
+
 const LandingPage = ({
 	currentUser,
+	tickets,
 }: {
 	currentUser: { id: string; email: string } | null;
+	tickets: Ticket[];
 }) => {
-	return currentUser ? (
-		<h1>You are signed in</h1>
-	) : (
-		<h1>You are NOT signed in</h1>
+	const ticketList = tickets.map((ticket) => {
+		return (
+			<tr key={ticket.id}>
+				<td>{ticket.title}</td>
+				<td>{ticket.price}</td>
+			</tr>
+		);
+	});
+
+	return (
+		<div>
+			<h1>Tickets</h1>
+			<table className='table'>
+				<thead>
+					<tr>
+						<th>Title</th>
+						<th>Price</th>
+					</tr>
+				</thead>
+				<tbody>{ticketList}</tbody>
+			</table>
+		</div>
 	);
 };
 
 LandingPage.getInitialProps = async (context, client, currentUser) => {
-	return {};
+	const { data } = await client.get('/api/tickets');
+
+	return { tickets: data as Ticket[] };
 };
 
 export default LandingPage;
