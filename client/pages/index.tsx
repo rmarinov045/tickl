38c6ad96d636f756626220a2d0b06d81b088a59e
Radostin Marinov <r.marinov045@gmail.com>
2022-07-02 15:57:14 +0300

    Add ticket data initial fetch

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import React from 'react';
 
-interface Ticket {
+export interface Ticket {
 	title: string;
 	price: number;
 	version: number;
--- client/pages/tickets/[ticketId].tsx
+++ client/pages/tickets/[ticketId].tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Ticket } from '../index';
+import useRequest from '../../hooks/useRequest';
 
-function TicketShow() {
-	return <div>TicketShow</div>;
+function TicketShow({ ticket }: { ticket: Ticket }) {
+	const { doRequest, errors } = useRequest({
+		url: '/api/orders',
+		method: 'post',
+		body: { ticketId: ticket.id },
+		onSuccess: (order) => console.log(order),
+	});
+
+	return (
+		<div>
+			<h1>{ticket.title}</h1>
+			<h4>Price: {ticket.price}</h4>
+			{errors}
+			<button onClick={doRequest} className='btn btn-primary'>
+				Purchase
+			</button>
+		</div>
+	);
 }
+
+TicketShow.getInitialProps = async (context, client) => {
+	const { ticketId } = context.query;
+	const { data } = await client.get(`/api/tickets/${ticketId}`);
+
+	return { ticket: data };
+};
 
 export default TicketShow;
